00715b60de184edd3e313b8c9373fcc08593e998
FaizanSarwar <faizansarwar.bs@gmail.com>
2022-10-03 00:57:42 -0700

    finally done

--- src/TableTask/InputFeilds.js
+++ src/TableTask/InputFeilds.js
@@ -1,55 +1,139 @@
-import React, {  useState } from 'react'
-import Table from './Table'
+import { Alert } from "bootstrap";
+import React, { useState ,useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import Table from "./Table";
 
+//get localdata from local storage
+const getLocalData = () => {
+  const list = localStorage.getItem("mylist");
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
 
 const InputFeilds = () => {
-    const [data, setData] = useState({
-        name: '',
-        email: '',
+  const [data, setData] = useState({
+    id: "",
+    name: "",
+    email: "",
+  });
+  const [table, setTable] = useState([]);
+  const [storeIdForUpdate, setStoreIdForUpdate] = useState();
+  const[changeBtn,setChangeBtn] =useState(false)
 
-    })
-    const [table, setTable] = useState([])
+  const inputEvent = (event) => {
+    const { name, value } = event.target;
+    setData((preventValue) => {
+      return {
+        ...preventValue,
+        [name]: value,
+      };
+    });
+    // console.log(data)
+  };
 
-    const inputEvent = (event) => {
-        const { name, value } = event.target;
-        setData((preventValue) => {
-            return {
-                ...preventValue,
-                [name]: value,
+  const handleButton = () => {
+    if(!data ){
+        alert("please fill all feilds")
+    }
+    else if (data &&changeBtn ){
+        setTable(
+        table.map((val)=>{
+          
+            if(val.id === storeIdForUpdate){
+                return {...val,name:data.name,email:data.email}
             }
-        })
-        // console.log(data)
-
-    }
-
-    const handleButton = () => { 
-        setTable([...table, data])
-        setData({  name: " ", email: " " })
-
-    }
-
-
-    return (
-        <div className='tbl d-flex'>
-            <div className='container col-lg-4 mt-5 ms-5 bg-dark py-5 px-5 '>
-                <div className="mb-3">
-                    <label className="form-label text-white">Name :</label>
-                    <input type="text" className="form-control" onChange={inputEvent} value={data.name} name="name" />
-
-                </div>
-                <div className="mb-3">
-                    <label className="form-label text-white" >Email :</label>
-                    <input type="email" className="form-control" onChange={inputEvent} value={data.email} name="email" />
-
-                </div>
-                <button className="btn btn-primary col-lg-12 mt-3" onClick={handleButton}>Submit</button>
-            </div>
+            return val;
             
-            <Table table={table} data={data} />
+        }))
+        setData({  name: " ", email: " " })
+        setChangeBtn(false)
+        console.log("inside if")
+    }
+    else{
+    data.id = uuid();
+    setTable([...table, data]);
+    setData({ name: " ", email: " " });
+    
+    console.log("inside else")
 
+   }
+  };
+  const deleteHandle = (id) => {
+    const updatedList = table.filter((curData) => {
+      return curData.id !== id;
+    });
+    setTable(updatedList);
+  };
+
+  const updateHandle = (id) => {
+    const findd = table.find((val) => {
+      return val.id === id;
+    });
+    setData({
+      name: findd.name,
+      email: findd.email,
+    });
+   setStoreIdForUpdate(id);
+   setChangeBtn(true)
+  
+  };
+
+
+   //set data in local storage...
+  //  useEffect(() => {
+  //   localStorage.setItem("mylist", JSON.stringify(table));
+  // }, [table]);
+
+
+  return (
+    <div className="tbl d-flex">
+      <div className="container col-lg-4 mt-5 ms-5 bg-dark py-5 px-5 ">
+        <div className="mb-3">
+          <label className="form-label text-white">Name :</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={inputEvent}
+            value={data.name}
+            name="name"
+          />
         </div>
+        <div className="mb-3">
+          <label className="form-label text-white">Email :</label>
+          <input
+            type="email"
+            className="form-control"
+            onChange={inputEvent}
+            value={data.email}
+            name="email"
+          />
+        </div>
+        {changeBtn ?  <button
+          className="btn btn-secondary col-lg-12 mt-3"
+          onClick={handleButton}
+        >
+          Update
+        </button> :
+        <button
+          className="btn btn-primary col-lg-12 mt-3"
+          onClick={handleButton}
+        >
+          Submit
+        </button>
+        }
+      </div>
 
-    )
-}
+      <Table
+        data={data}
+        table={table}
+        deleteHandle={deleteHandle}
+        updateHandle={updateHandle}
+      />
+    </div>
+  );
+};
 
-export default InputFeilds
+export default InputFeilds;
--- src/TableTask/Table.js
+++ src/TableTask/Table.js
@@ -1,40 +1,53 @@
-import React from 'react'
+import React from "react";
 
-const Table = ({table,data}) => {
+const Table = ({ table, deleteHandle, updateHandle }) => {
   return (
     <>
-    <table class="table mt-5 mx-5">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    {data ? table.map((val,index)=>{
-            return(
-                <>
+      <table class="table mt-5 mx-5">
+        <thead>
+          <tr>
+            <th scope="col">Sr.</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table.map((val, index) => {
+            return (
+              <>
                 <tr>
-             <th scope="row" key={index}>{index+1}</th>
-             <td>{val.name}</td>
-             <td>{val.email}</td>
-             <td>
-             <button type="button" class="btn btn-warning me-2">Edit</button>
-                <button type="button" class="btn btn-danger">Delete</button>          
-             </td>
-             </tr>         
-             </>
+                  <th scope="row" key={index}>
+                    {index + 1}
+                  </th>
+                  <td>{val.name}</td>
+                  <td>{val.email}</td>
 
-       ) }) :  <h1>NO DATA</h1>}
-   
-        
-  </tbody>
-</table>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-warning me-2"
+                      onClick={() => updateHandle(val.id)}
+                    >
+                      Edit
+                    </button>
+
+                    <button
+                      type="button"
+                      class="btn btn-danger"
+                      onClick={() => deleteHandle(val.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </>
+            );
+          })}
+        </tbody>
+      </table>
     </>
-    
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
